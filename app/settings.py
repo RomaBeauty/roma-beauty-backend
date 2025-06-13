7e29eef3417e5dac4f18d6bc2944811ca892fa35
Fefrancine <fernanda.francine.f@gmail.com>
2025-06-13 18:54:15 -0300

    fix: update secret key

--- app/settings.py
+++ app/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django_filters',
     'drf_spectacular',
     'rest_framework',
+    'corsheaders',
+    'api',
     'core',
 ]
 
@@ -51,7 +53,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 ROOT_URLCONF = 'app.urls'
 
