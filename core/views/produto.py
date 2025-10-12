0c7d9c1221d1dbc0e1539cc55607f6790797c6de
gustavomurar <gustavomurarosoares3@gmail.com>
2025-10-12 20:01:53 -0300

    feat: Menu working with the backend.

--- core/views/produto.py
+++ core/views/produto.py
@@ -1,8 +1,16 @@
 # core/views/produto.py
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from core.models.produto import Produto
 from core.serializers.produto import ProdutoSerializer
 
 class ProdutoViewSet(viewsets.ModelViewSet):
     queryset = Produto.objects.select_related('category', 'colecao', 'tipo').all()
     serializer_class = ProdutoSerializer
+
+class ProdutoDetail(APIView):
+    def get(self, request, pk):
+        produto = Produto.objects.get(pk=pk)
+        serializer = ProdutoSerializer(produto)
+        return Response(serializer.data)
